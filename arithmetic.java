4f633c2e076e250e36bf36386d22e9ee17109712
divy-patel15 <divypatel1526@gmail.com>
2026-02-02 21:52:13 +0530

    Create arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,19 @@
+public class arithmetic
+{
+    public static void main(String[] args)
+     {
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int division = a / b;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + division);
+    }
+    
+}
