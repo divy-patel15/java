2316c903a8f8deede788c4f7a91a117e05d878ea
divy-patel15 <divypatel1526@gmail.com>
2026-02-03 17:53:26 +0530

    Create Conditional.class

---
+++ odd_even.java
@@ -0,0 +1,16 @@
+class odd_even
+{
+    public static void main(String[] args) 
+    {
+        int number = 7;
+
+        if (number % 2 == 0) 
+        {
+          System.out.println(number + " is even.");
+        } 
+        else 
+        {
+          System.out.println(number + " is odd.");
+        }
+    }
+}
